3698cd9acc644b4904cf8a1940c8360ad8c3bed9
Andrey000001 <116907910+Andrey000001@users.noreply.github.com>
2024-01-20 19:48:58 +0000

    Deploying to gh-pages from @ Andrey000001/allfilms@f9e3cfec0a5609b0b3c6af25f5b6a5dd8728ccb6 🚀

---
+++ index.c6443d0e.js
@@ -0,0 +1,11 @@
+const e="8c1c21cb3de0f31fcce2cce049e2c70c";async function t(t=1){let a=await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${e}&page=${t}`),i=await a.json();return Promise.all(await i.results.map(async t=>{try{let a=await fetch(`https://api.themoviedb.org/3/movie/${t.id}?api_key=${e}&include_image_language=en,null`);if(!a.ok)throw Error(a.statusText);return await a.json()}catch(e){console.log("Errorrrrrr",e)}}))}i();const a=document.querySelector(".js-cards");async function i(){try{let e=(await t()).filter(e=>{if(e&&null!==e.poster_path)return e}).map(({title:e,poster_path:t,genres:a,release_date:i})=>{let r=a.map(e=>e.name).join(", "),s=i.slice(0,4);return`
+            <li class="page-item">
+               <a href="#" class="page-item__link">
+               <img src="https://image.tmdb.org/t/p/w500${t}">
+               <div class="page-description">
+                   <h2 class="page-description__title">${e}</h2>
+                   <p class="page-description__podscription">${r} <span>| ${s}</span></p>
+               </div>
+               </a>
+            </li>`}).join(" ");a.insertAdjacentHTML("beforeend",e)}catch(e){console.log(e)}}
+//# sourceMappingURL=index.c6443d0e.js.map
